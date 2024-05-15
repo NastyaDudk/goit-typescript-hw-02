89f1d5e2ab1ab263d401a54f284c68af1c354082
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-05-15 11:38:33 +0400

    ok

---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import { GoSearch } from "react-icons/go";
+import { SearchbarProps } from "../App.types";
+import css from "./Searchbar.module.css";
+
+
+const Searchbar: React.FC<SearchbarProps> = ({ onSubmit }) => {
+  const [searchInputText, setSearchInputText] = useState<string>("");
+
+  const handleSearchbarSubmit = (evt: React.FormEvent): void => {
+    evt.preventDefault();
+    onSubmit(searchInputText);
+  };
+
+  const handleSearchbarInputChange = (
+    evt: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    evt.preventDefault();
+    const {
+      target: { value },
+    } = evt;
+    setSearchInputText(value);
+  };
+
+  return (
+    <>
+      <header className={css.searchBar}>
+        <form className={css.searchForm} onSubmit={handleSearchbarSubmit}>
+          <button type="submit" className={css.searchFormButton}>
+      
+          <GoSearch />
+          </button>
+          <input
+            className={css.searchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos..."
+            onChange={handleSearchbarInputChange}
+            value={searchInputText}
+          />
+        </form>
+      </header>
+    </>
+  );
+};
+
+export default Searchbar;
